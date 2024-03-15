c9027589ec2c1a412e2ce85d09359c958b28b35c
AllenLin3 <allen13572@gmail.com>
2024-03-15 11:42:30 +0800

    week04

---
+++ week04/week04-2.py
@@ -0,0 +1,3 @@
+#week04-2
+for i in range(1,3+1):
+  print('i是',i)
---
+++ week04/week04-3.py
@@ -0,0 +1,7 @@
+#week04-3
+#for i in range(1,10):
+for i in range(1,9+1,+1):
+  print('i是',i,end=' ')
+print()
+for i in range(9,1-1,-1):
+  print('i是',i,end=' ')
